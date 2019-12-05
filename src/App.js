b187fa4bbb8e9b1a7b7c5778800fbc4c173cdce1
Ivan5 <converse_ivan@hotmail.com>
2019-12-05 12:14:49 -0600

    Make a Register component and refactor some code in others components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
@@ -22,7 +23,8 @@ class App extends React.Component {
       input: "",
       imageUrl: "",
       box: {},
-      route: "signin"
+      route: "signin",
+      isSignedIn: false
     };
   }
 
@@ -60,14 +62,24 @@ class App extends React.Component {
       .catch(err => console.log(err));
   };
 
+  onRouteChange = route => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
   render() {
     return (
       <div className="App">
         <Particles className="particles" />
-        <Navigation />
-        {this.state.route === "signin" ? (
-          <Signin />
-        ) : (
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {this.state.route === "home" ? (
           <>
             <Logo />
             <Rank />
@@ -80,6 +92,10 @@ class App extends React.Component {
               imageUrl={this.state.imageUrl}
             />{" "}
           </>
+        ) : this.state.route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
         )}
       </div>
     );
