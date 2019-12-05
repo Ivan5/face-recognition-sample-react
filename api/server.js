7d0610d4a356ef4145d51d0ee6fb4807e71fb50d
Ivan5 <converse_ivan@hotmail.com>
2019-12-05 14:42:52 -0600

    Add and profile and image route

--- api/server.js
+++ api/server.js
@@ -55,6 +55,39 @@ app.post("/register", (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
+app.get("/profile/:id", (req, res) => {
+  const id = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    } else {
+      res.status(400).json("No such user");
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const id = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    } else {
+      res.status(400).json("No such user");
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
 app.listen(3000, () => {
   console.log("Running...");
 });
