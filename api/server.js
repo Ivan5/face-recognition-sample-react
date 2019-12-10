d689abd11089f9e8c6eea17172aae4bf2e91fe9b
Ivan5 <converse_ivan@hotmail.com>
2019-12-10 16:12:50 -0600

    Connecting to the database with knex.js

--- api/server.js
+++ api/server.js
@@ -2,10 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
+const knex = require("knex");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+const postgres = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "root",
+    database: "smart-brain"
+  }
+});
+
+console.log(postgres.select("*").from("users"));
 
 const database = {
   users: [
